b145e8b3778198b969edbab876a124fd2f5cf06b
turtlecute33 <1satoshi@riseup.net>
2023-11-06 19:14:10 +0100

    Script for clean your linux system

---
+++ clean-script.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# Update package repositories (may vary by distribution)
+if [ -x "$(command -v apt-get)" ]; then
+  sudo apt-get update
+elif [ -x "$(command -v dnf)" ]; then
+  sudo dnf check-update
+elif [ -x "$(command -v yum)" ]; then
+  sudo yum check-update
+elif [ -x "$(command -v zypper)" ]; then
+  sudo zypper refresh
+fi
+
+# Upgrade system packages
+if [ -x "$(command -v apt-get)" ]; then
+  sudo apt-get -y upgrade
+elif [ -x "$(command -v dnf)" ]; then
+  sudo dnf -y upgrade
+elif [ -x "$(command -v yum)" ]; then
+  sudo yum -y update
+elif [ -x "$(command -v zypper)" ]; then
+  sudo zypper update
+fi
+
+# Remove orphaned packages (may vary by distribution)
+if [ -x "$(command -v apt-get)" ]; then
+  sudo apt-get autoremove --purge
+elif [ -x "$(command -v dnf)" ]; then
+  sudo dnf autoremove
+elif [ -x "$(command -v yum)" ]; then
+  sudo package-cleanup --leaves
+elif [ -x "$(command -v zypper)" ]; then
+  sudo zypper packages --unneeded
+fi
+
+# Clean package cache
+if [ -x "$(command -v apt-get)" ]; then
+  sudo apt-get clean
+elif [ -x "$(command -v dnf)" ]; then
+  sudo dnf clean packages
+elif [ -x "$(command -v yum)" ]; then
+  sudo yum clean all
+elif [ -x "$(command -v zypper)" ]; then
+  sudo zypper clean --all
+fi
+
+# Remove old logs (may vary by distribution)
+sudo journalctl --vacuum-time=3d
+
+# Clean temporary files
+sudo rm -rf /tmp/*
+
+# Clean thumbnail cache
+rm -rf ~/.cache/thumbnails/*
+
+# Remove old kernels (may vary by distribution)
+if [ -x "$(command -v apt-get)" ]; then
+  sudo apt-get autoremove --purge
+elif [ -x "$(command -v dnf)" ]; then
+  sudo package-cleanup --oldkernels --count=1
+elif [ -x "$(command -v yum)" ]; then
+  sudo package-cleanup --oldkernels --count=1
+elif [ -x "$(command -v zypper)" ]; then
+  sudo zypper remove-old-kernels --keep 1
+fi
+
+# Clean up unneeded locale files
+localedef --list-archive | grep -v 'en_US\|C' | xargs localedef --delete-from-archive
+
+# Remove old logs in /var/log
+sudo find /var/log -type f -name '*.gz' -exec rm -f {} \;
+sudo find /var/log -type f -name '*.1' -exec rm -f {} \;
+
+# Remove cached package data (apt)
+if [ -x "$(command -v apt-get)" ]; then
+  sudo apt-get clean
+  sudo rm -rf /var/lib/apt/lists/*
+fi
+
+# Print a message
+echo "Aggressive system cleanup and optimization completed."
+
+
